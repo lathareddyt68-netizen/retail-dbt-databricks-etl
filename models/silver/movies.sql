0d5c9f48a8f39612cf3dad2e6655a037491bd0d9
Latha Thippireddy <lathareddyt68@gmail.com>
2026-07-02 14:48:13 +0530

    adding etl audit columns

--- models/silver/movies.sql
+++ models/silver/movies.sql
@@ -5,11 +5,18 @@
 ) }}    
 
 WITH source_data AS (
-    
+
 SELECT *
 FROM {{ ref('stg_mymoviedb') }}
 WHERE record_status = 'VALID'
 )
 
-SELECT *
+SELECT
+    *,
+    '{{ var("batch_id") }}' AS batch_id,
+    '{{ var("pipeline_name") }}' AS pipeline_name,
+    '{{ var("pipeline_run_id") }}' AS pipeline_run_id,
+    CURRENT_TIMESTAMP() AS load_timestamp,
+    CURRENT_DATE() AS batch_date
+
 FROM source_data
